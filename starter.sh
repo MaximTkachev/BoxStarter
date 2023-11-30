1b6f5789d8de571263637fa4b79f7ba9550513f0
MaximTkachev <69761681+MaximTkachev@users.noreply.github.com>
2023-12-01 00:42:35 +0700

    add shell script

---
+++ starter.sh
@@ -0,0 +1,8 @@
+apt update -y && \
+apt upgrade -y && \
+apt install -y default-jre && \
+apt install -y maven && \
+apt install -y docker.io && \
+apt install -y docker-compose && \
+clear && \
+printf "All the software is installed.\nEnjoy your use!\n"
